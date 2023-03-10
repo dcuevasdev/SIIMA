5f1d31b31e83a9ad25465c88c6ba4725410f5481
Daniel Cuevas <ivandaniel1224@gmail.com>
2023-03-10 10:42:09 -0500

    Header menu code

--- src/JavaScript/index.js
+++ src/JavaScript/index.js
@@ -1,1 +1,13 @@
+const buttonMenu = document.querySelector(".header__button");
+const navMenu = document.querySelector(".header__nav");
+const itemsMenu = [...document.querySelectorAll(".list__item")];
 
+buttonMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("menu-visible");
+});
+
+itemsMenu.forEach((item) => {
+  item.addEventListener("click", () => {
+    navMenu.classList.remove("menu-visible");
+  });
+});
