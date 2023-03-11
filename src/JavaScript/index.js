9f2447a77216d801ea166b014999624065847f87
Daniel Cuevas <ivandaniel1224@gmail.com>
2023-03-11 11:37:40 -0500

    Carrousel styles

--- src/JavaScript/index.js
+++ src/JavaScript/index.js
@@ -2,6 +2,9 @@ const buttonMenu = document.querySelector(".header__button");
 const navMenu = document.querySelector(".header__nav");
 const itemsMenu = [...document.querySelectorAll(".list__item")];
 
+const slider = document.querySelector(".carrousel__img-photo");
+
+//Toggle Menu
 buttonMenu.addEventListener("click", () => {
   navMenu.classList.toggle("menu-visible");
 });
